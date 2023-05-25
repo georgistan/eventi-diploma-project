d6412a51a2269b40dac334c659287db38ad2042c
Georgi Stanachkov <georgistan@abv.bg>
2023-05-25 20:50:22 +0300

    home_screen
    Edit: Small changes to the UI

--- app/src/main/java/com/example/eventi/ui/app/screens/HomeScreen.kt
+++ app/src/main/java/com/example/eventi/ui/app/screens/HomeScreen.kt
@@ -16,7 +16,6 @@ import com.example.eventi.navigation.BottomNavigation
 import com.example.eventi.navigation.navigateToSingleEvent
 import com.example.eventi.ui.app.components.home_screen.HomeContent
 import com.example.eventi.viewmodels.HomeViewModel
-import timber.log.Timber
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
